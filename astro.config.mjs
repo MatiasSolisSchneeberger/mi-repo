1ab21ca80e86f0f9aadb85d43f80fd61b3d7a978
MatiasSolisSchneeberger <Matias.Solis.Sch@gmail.com>
2024-07-24 00:27:08 -0300

    sacando mi-repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https:matiassolisschneeberger.github.io',
-  base: 'mi-repo',
   integrations: [tailwind(), react()]
 });
