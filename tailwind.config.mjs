a7b314ff23d50258306aa466e8053446dd5aa0f6
MatiasSolisSchneeberger <Matias.Solis.Sch@gmail.com>
2024-08-30 12:33:50 -0300

    30/08/24

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import("tailwindcss").Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}","./node_modules/flowbite/**/*.js"],
 	theme: {
 		extend: {
 			backgroundImage:{
@@ -306,6 +306,8 @@ export default {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+        require('flowbite/plugin')
+    ],
 	darkMode: "class"
 }
