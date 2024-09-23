53ecfec5dc34c6f5286a303fa2e3c4f629e9e7fa
MatiasSolisSchneeberger <Matias.Solis.Sch@gmail.com>
2024-09-22 21:24:01 -0300

    vercel no

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https:matiassolisschneeberger.github.io',
   base: 'swifties-eras-tour',
-  integrations: [tailwind({
-      applyBaseStyles: false,
-    }),
-    react()],
-  output: "server",
-  adapter: vercel()
+  integrations: [tailwind(), react()],
 });
